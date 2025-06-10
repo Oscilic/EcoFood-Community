60ceee33bc25ecf8ce0fda0d96ab7de9de9f8a0a
Paul Fernández <paulfernandezb@gmail.com>
2025-06-10 13:30:02 -0400

    Fix para ejecución en localhost

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+
+import { useEffect } from 'react';
 import CardProducto from '../components/CardProducto';
 import { getUserData } from "../services/userService";
 import { useAuth } from "../context/AuthContext";
 
 function Home() {
+    /*const { user } = useAuth();
+
+    useEffect(() => {
+        const fetch = async () => {
+            const datos = await getUserData(user.uid);
+            setUserData(datos);
+        };
+        if (user) fetch();
+    }, [user]);*/
+
     return (
         <div className="container mt-4">
             <h1>Productos Disponibles</h1>
@@ -10,12 +22,4 @@ function Home() {
         </div>
     );
 }
-useEffect(() => {
-    const fetch = async () => {
-        const datos = await getUserData(user.uid);
-        setUserData(datos);
-    };
-    if (user) fetch();
-}, [user]);
-
 export default Home;
--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 function AppRouter() {
     return (
     <BrowserRouter>
